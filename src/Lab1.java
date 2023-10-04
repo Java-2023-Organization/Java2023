a0ca566172a53d5384ca38c687a47384a028bc37
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-10-04 10:34:48 +0300

    add lab 1

---
+++ src/Lab1.java
@@ -0,0 +1,20 @@
+/**
+ * My program for first laboratory work
+ */
+public class Lab1 {
+    /**
+     * Standard entry point (method) for Java
+     *
+     * Variant: 6101
+     *
+     * @param args - standard arguments for passing values in Java program by console standard
+     */
+    public static void main(String[] args) {
+        System.err.println("RESULT:");
+        System.out.println(args[2]);
+        System.out.println("\u001B[35m123123");
+
+        String s = "ASD";
+        System.out.println(s.substring(1));
+    }
+}

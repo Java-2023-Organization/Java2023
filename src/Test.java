df2ce17d6aa62e3ec8ab8f5e4f25a9e9e3a10d99
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-12-20 11:01:34 +0200

    upd lab4 (nested==static vs. inner vs. embedded classes)

---
+++ src/Test.java
@@ -0,0 +1,5 @@
+import lab4.Student;
+
+public class Test {
+    private static Student.Main main;
+}
--- src/lab4/Student.java
+++ src/lab4/Student.java
@@ -1,26 +1,38 @@
 package lab4;
 
 //todo check "final" key word
+//todo nested==static vs. inner vs. embedded classes
 public class Student {
     private String name;
     static private Main main;
 
     public static void main(String[] args) {
+        main.main(new String[] {});
         Student student = new Student();
-        Main main1;
+//        InternalMain main1;
 
 //        todo check why it is needed??))) and what is the name of this case?
 //        todo is it possible to use method/function inside other methode/function (except lambda case)
-        class Main {
+        class InternalMain {
             public static void main(String[] args) {
+                System.out.println("InternalMain");
                 Student student = new Student();
             }
         }
+
+        InternalMain.main(new String[] {});
+        InternalMain main2 = null;
+        main2.main(new String[] {});
     }
 
+    /*static void method() {
+        main.Internal
+    }*/
+
 //    todo check why static is needed
-    /*static*/ class Main {
+    public /*static*/ class Main {
         public static void main(String[] args) {
+            System.out.println("Main.main()");
             Student student = new Student();
         }
     }
